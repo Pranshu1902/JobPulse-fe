1a1bf59dcdeef0f720a53b71a7d8fdf0ae5bf0dd
Pranshu1902 <pranshu1902@gmail.com>
2024-06-06 00:47:05 +0530

    integrate api

---
+++ app/api/fetch.ts
@@ -0,0 +1,48 @@
+// const baseURL = "https://jobpulse.com/";
+const baseURL = "http://127.0.0.1:8000/";
+
+type methods = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+
+// main request function
+export async function request(
+  method: methods = "GET",
+  data: any = {},
+  endpoint: string,
+  token?: string
+) {
+  let url;
+  let payload: string;
+
+  if (method === "GET") {
+    const requestParams = data
+      ? `?${Object.keys(data)
+          .map((key) => `${key}=${data[key]}`)
+          .join("&")}`
+      : "";
+    url = `${baseURL}${endpoint}${requestParams}`;
+    payload = "";
+  } else {
+    url = `${baseURL}${endpoint}`;
+    payload = data ? JSON.stringify(data) : "";
+  }
+
+  // Token Authentication
+  const auth = token ? "Token " + token : "";
+
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: { "Content-Type": "application/json", Authorization: auth },
+      body: method !== "GET" ? payload : null,
+    });
+    if (response.ok) {
+      const json = await response.json();
+      return json;
+    } else {
+      const errorJson = await response.json();
+      throw Error(errorJson);
+    }
+  } catch (error) {
+    return error;
+  }
+}
