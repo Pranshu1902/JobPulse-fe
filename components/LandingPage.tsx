72c216ea307ed01139501d57b68bd70227d3051e
Pranshu1902 <pranshu1902@gmail.com>
2024-06-10 14:49:11 +0530

    update landing page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,8 +1,33 @@
+import Image from "next/image";
+import logo from "@assets/plain_logo.png";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
-    <div>
-      <div>JobPulse</div>
-      <p>One stop solution</p>
+    <div className="p-4">
+      {/* Header */}
+      <div className="flex justify-between">
+        <div className="flex items-center gap-6">
+        <Image src={logo} alt="logo" width={50}></Image>
+        <p>JobPulse</p>
+        <a href="#about">About Us</a>
+        </div>
+        <div className="flex items-center gap-4">
+          <Link href={"/login"} className="p-2 rounded-lg">
+            Login
+          </Link>
+          <Link
+            href={"/signup"}
+            className="p-2 rounded-lg border border-purple-600"
+          >
+            Signup
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-6">
+        <p className="text-6xl font-bold p-6">Stay Ahead of the Game with JobPulse</p>
+      </div>
     </div>
   );
 }
