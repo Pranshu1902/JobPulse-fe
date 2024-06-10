9a3278e3a3115b302db285f2f51ffae2a752de41
Pranshu1902 <pranshu1902@gmail.com>
2024-06-11 00:44:24 +0530

    add jobs page board

---
+++ app/jobs/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { JOB_STATUSES } from "@constants/constants";
+import { request } from "@api/fetch";
+import { useCookies } from "next-client-cookies";
+import { useEffect, useState } from "react";
+import { Job, JobStatus } from "@models/types";
+
+export default function Jobs() {
+  const cookies = useCookies();
+  const [jobList, setJobList] = useState<Job[]>([]);
+
+  const showBoardColumn = (status: JobStatus) => {
+    return (
+      <div className="p-3 rounded-xl bg-primary w-1/4 text-white">
+        <div>
+          <p className="font-semibold text-2xl mb-4">{status}</p>
+        </div>
+        <div>
+          {jobList
+            .filter((job: Job) => job.status.status === status)
+            .map((job: Job) => showJobCard(job))}
+        </div>
+      </div>
+    );
+  };
+
+  const showJobCard = (job: any) => {
+    return (
+      <div className="bg-secondary p-3 rounded-lg">
+        <b className="text-xl">{job.role}</b>
+        <p>{job.company.name}</p>
+        <p>{job.contract_length}</p>
+        <p>Salary: {job.salary}</p>
+      </div>
+    );
+  };
+
+  const fetchData = async () => {
+    const response = await request("GET", {}, "jobs/", cookies.get("token"));
+    setJobList(response);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="p-4">
+      <div>
+        <p className="text-2xl">List Jobs</p>
+      </div>
+      <div className="flex overflow-scroll gap-4">
+        {JOB_STATUSES.map((job_status: JobStatus) =>
+          showBoardColumn(job_status)
+        )}
+      </div>
+    </div>
+  );
+}
