1513fa5f21894d5c996f197b93bd427bc16e9fa2
Pranshu1902 <pranshu1902@gmail.com>
2024-06-13 02:26:46 +0530

    add back link on new job details page

--- app/jobs/[job_id]/page.tsx
+++ app/jobs/[job_id]/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { useParams } from 'next/navigation';
+import { useParams } from "next/navigation";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 
 export default function JobDetail() {
+  const router = useRouter();
   const params = useParams();
   const jobId = params.job_id;
 
@@ -11,9 +15,14 @@ export default function JobDetail() {
   }
 
   return (
-    <div>
-      <div>
-        <p>Job Details</p>
+    <div className="p-4">
+      <div className="mb-4">
+        <div className="flex items-center gap-2 font-semibold text-2xl">
+          <button onClick={() => router.back()}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <p>Job Details</p>
+        </div>
         <p>{jobId}</p>
       </div>
     </div>
