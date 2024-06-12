da179e30ca3bdd8ff3bfee7daf74c29668618008
Pranshu1902 <pranshu1902@gmail.com>
2024-06-13 02:26:36 +0530

    add back link on new job page

--- app/jobs/new/page.tsx
+++ app/jobs/new/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
+
 export default function NewJob() {
+  const router = useRouter();
+
   return (
-    <div>
-      <div>
-        <p>New Job</p>
+    <div className="p-4">
+      <div className="mb-4">
+        <div className="flex items-center gap-2 font-semibold text-2xl">
+          <button onClick={() => router.back()}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <p className="font-semibold text-2xl">Add New Job</p>
+        </div>
       </div>
+      <form>
+        <p>Role:</p>
+        <input className="border border-primary" type="text" name="" id="" />
+        <p>Company:</p>
+        <input className="border border-primary" type="text" name="" id="" />
+        <p>Platform:</p>
+        <input className="border border-primary" type="text" name="" id="" />
+      </form>
     </div>
   );
 }
