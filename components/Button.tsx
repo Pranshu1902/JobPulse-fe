75fba3fb4e6c5ca74bb927857565d88e945f315b
Pranshu1902 <pranshu1902@gmail.com>
2024-06-16 20:46:24 +0530

    add a button component

---
+++ components/Button.tsx
@@ -0,0 +1,25 @@
+import { ButtonTypes } from "@models/types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface ButtonProps {
+  text: string;
+  onClick: (e: any) => void;
+  type: ButtonTypes;
+  icon?: any;
+}
+
+export default function Button(props: ButtonProps) {
+  return (
+    <button
+      className={`${
+        props.type === "primary"
+          ? "bg-primary text-white"
+          : "border border-primary"
+      } font-bold p-2 rounded-lg px-12 py-2 flex justify-center items-center`}
+      onClick={props.onClick}
+    >
+      <FontAwesomeIcon icon={props?.icon} />
+      {props.text}
+    </button>
+  );
+}
