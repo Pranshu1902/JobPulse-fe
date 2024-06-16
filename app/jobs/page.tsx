b598c9297a68573e5a9bc8e377355bb716ac6a08
Pranshu1902 <pranshu1902@gmail.com>
2024-06-16 23:07:48 +0530

    add navigation arrows

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -3,17 +3,23 @@
 import { JOB_STATUSES } from "@constants/constants";
 import { request } from "@api/fetch";
 import { useCookies } from "next-client-cookies";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Job, JobStatus } from "@/models/models";
 import Link from "next/link";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Jobs() {
   const cookies = useCookies();
   const [jobList, setJobList] = useState<Job[]>([]);
+  const scrollRef = useRef<HTMLDivElement>(null);
 
   const showBoardColumn = (status: JobStatus) => {
     return (
-      <div className="p-3 rounded-xl bg-primary min-w-[300px] w-[300px] text-white flex flex-col max-h-[80vh]">
+      <div
+        key={status}
+        className="p-3 rounded-xl bg-primary min-w-[300px] w-[300px] text-white flex flex-col max-h-[80vh]"
+      >
         <div>
           <p className="font-semibold text-2xl mb-4">{status}</p>
         </div>
@@ -28,7 +34,7 @@ export default function Jobs() {
 
   const showJobCard = (job: any) => {
     return (
-      <div className="bg-secondary p-3 rounded-lg">
+      <div key={job.id} className="bg-secondary p-3 rounded-lg">
         <Link href={`/jobs/${job.id}`}>
           <b className="text-xl">{job.role}</b>
           <p>{job.company.name}</p>
@@ -48,6 +54,18 @@ export default function Jobs() {
     fetchData();
   }, []);
 
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft -= 200; // Adjust scrolling distance as needed
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += 200; // Adjust scrolling distance as needed
+    }
+  };
+
   return (
     <div className="p-4">
       <div className="flex justify-between items-center mb-4">
@@ -59,10 +77,24 @@ export default function Jobs() {
           + New
         </Link>
       </div>
-      <div className="flex gap-4 overflow-x-auto">
-        {JOB_STATUSES.map((job_status: JobStatus) =>
-          showBoardColumn(job_status)
-        )}
+      <div className="flex gap-4 relative">
+        <button
+          className="absolute px-3 left-1 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2"
+          onClick={scrollLeft}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <div className="flex gap-4 overflow-x-auto" ref={scrollRef}>
+          {JOB_STATUSES.map((job_status: JobStatus) =>
+            showBoardColumn(job_status)
+          )}
+        </div>
+        <button
+          className="absolute px-3 right-1 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2"
+          onClick={scrollRight}
+        >
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
       </div>
     </div>
   );
