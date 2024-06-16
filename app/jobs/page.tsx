5cce33c7956fd56a61aeddf528abd6930fde00ae
Pranshu1902 <pranshu1902@gmail.com>
2024-06-16 23:01:14 +0530

    fix ui of kanban board on jobs page

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -13,11 +13,11 @@ export default function Jobs() {
 
   const showBoardColumn = (status: JobStatus) => {
     return (
-      <div className="p-3 rounded-xl bg-primary w-1/4 text-white">
+      <div className="p-3 rounded-xl bg-primary min-w-[300px] w-[300px] text-white flex flex-col max-h-[80vh]">
         <div>
           <p className="font-semibold text-2xl mb-4">{status}</p>
         </div>
-        <div>
+        <div className="flex flex-col gap-6 overflow-y-auto pb-3">
           {jobList
             .filter((job: Job) => job.status.status === status)
             .map((job: Job) => showJobCard(job))}
@@ -59,7 +59,7 @@ export default function Jobs() {
           + New
         </Link>
       </div>
-      <div className="flex overflow-scroll gap-4">
+      <div className="flex gap-4 overflow-x-auto">
         {JOB_STATUSES.map((job_status: JobStatus) =>
           showBoardColumn(job_status)
         )}
