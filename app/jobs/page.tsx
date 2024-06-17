a1ef57d3b26cde42936d7a964224dde3ea0490f3
Pranshu1902 <pranshu1902@gmail.com>
2024-06-17 22:00:01 +0530

    display count for each status

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -8,30 +8,38 @@ import { Job, JobStatus } from "@/models/models";
 import Link from "next/link";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "@/components/Button";
 
 export default function Jobs() {
   const cookies = useCookies();
-  const [jobList, setJobList] = useState<Job[]>([]);
+  const [filteredJobList, setFilteredJobList] = useState<{
+    [key: string]: Job[];
+  }>({
+    Applied: [],
+    Offered: [],
+    Rejected: [],
+    Withdrawn: [],
+    Accepted: [],
+  });
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const showBoardColumn = (status: JobStatus) => {
-    const filteredJobs = jobList.filter(
-      (job: Job) => job.status.status === status
-    );
-
     return (
       <div
         key={status}
-        className="p-3 rounded-xl bg-primary min-w-[300px] w-[300px] text-white flex flex-col max-h-[80vh]"
+        className="p-4 rounded-xl bg-lightgray min-w-[300px] w-[300px] flex flex-col max-h-[80vh]"
       >
-        <div>
-          <p className="font-semibold text-2xl mb-4">{status}</p>
+        <div className="flex justify-between items-center mb-4">
+          <p className="font-semibold text-2xl">{status}</p>
+          <p className="p-2 px-4 text-xl rounded-full bg-primary text-lightbg font-bold">
+            {filteredJobList[status].length}
+          </p>
         </div>
         <div className="flex flex-col gap-6 overflow-y-auto pb-3">
-          {filteredJobs.map((job: Job) => showJobCard(job))}
+          {filteredJobList[status].map((job: Job) => showJobCard(job))}
         </div>
-        {filteredJobs.length === 0 && (
-          <div className="flex justify-center p-2 bg-secondary rounded-lg">
+        {filteredJobList[status].length === 0 && (
+          <div className="flex justify-center p-2 bg-lightbg rounded-lg">
             No Jobs found
           </div>
         )}
@@ -41,12 +49,19 @@ export default function Jobs() {
 
   const showJobCard = (job: any) => {
     return (
-      <div key={job.id} className="bg-secondary p-3 rounded-lg">
-        <Link href={`/jobs/${job.id}`}>
+      <div key={job.id} className="bg-lightbg p-3 rounded-lg">
+        <div>
           <b className="text-xl">{job.role}</b>
           <p>{job.company.name}</p>
           <p>{job.contract_length}</p>
           <p>Salary: {job.salary}</p>
+        </div>
+        <Link href={`/jobs/${job.id}`}>
+          <Button
+            className="mt-4"
+            type="secondary"
+            text="View Details"
+          ></Button>
         </Link>
       </div>
     );
@@ -54,7 +69,21 @@ export default function Jobs() {
 
   const fetchData = async () => {
     const response = await request("GET", {}, "jobs/", cookies.get("token"));
-    setJobList(response);
+
+    // sort by status
+    const organizedJobs = response.reduce(
+      (acc: { [key: string]: Job[] }, job: Job) => {
+        const status = job.status.status;
+        if (!acc[status]) {
+          acc[status] = [];
+        }
+        acc[status].push(job);
+        return acc;
+      },
+      { Applied: [], Offered: [], Rejected: [], Withdrawn: [], Accepted: [] }
+    );
+
+    setFilteredJobList(organizedJobs);
   };
 
   useEffect(() => {
@@ -63,29 +92,27 @@ export default function Jobs() {
 
   const scrollLeft = () => {
     if (scrollRef.current) {
-      scrollRef.current.scrollBy({ left: -300, behavior: 'smooth' });
+      scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
     }
   };
 
   const scrollRight = () => {
     if (scrollRef.current) {
-      scrollRef.current.scrollBy({ left: 300, behavior: 'smooth' });    }
+      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
   };
 
   return (
     <div className="p-4">
-      <div className="flex justify-between items-center mb-4">
-        <p className="text-2xl">List Jobs</p>
-        <Link
-          href={"/jobs/new"}
-          className="border border-primary p-2 rounded-lg px-6"
-        >
-          + New
+      <div className="flex justify-between items-center mb-8">
+        <p className="text-3xl font-bold">List Jobs</p>
+        <Link href={"/jobs/new"}>
+          <Button text="+ New" type="primary" />
         </Link>
       </div>
       <div className="flex gap-4 relative">
         <button
-          className="absolute px-3 left-1 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2"
+          className="absolute px-3 left-1 top-1/2 transform -translate-y-1/2 bg-tertiary bg-opacity-50 rounded-full p-2"
           onClick={scrollLeft}
         >
           <FontAwesomeIcon icon={faArrowLeft} />
@@ -96,7 +123,7 @@ export default function Jobs() {
           )}
         </div>
         <button
-          className="absolute px-3 right-1 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 rounded-full p-2"
+          className="absolute px-3 right-1 top-1/2 transform -translate-y-1/2 bg-tertiary bg-opacity-50 rounded-full p-2"
           onClick={scrollRight}
         >
           <FontAwesomeIcon icon={faArrowRight} />
