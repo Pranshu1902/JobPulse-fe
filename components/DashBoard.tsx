58d2d4779b4e117751ad8f390a63189757524f3b
Pranshu1902 <pranshu1902@gmail.com>
2024-06-19 21:37:54 +0530

    revert to prev dashboard

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -6,18 +6,13 @@ import { useRouter } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
-  faSignOutAlt,
   faUser,
   faUserGraduate,
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import Logo from "@components/Logo";
 
-export default function DashBoard({
-  onLinkClick,
-}: {
-  onLinkClick: () => void;
-}) {
+export default function DashBoard({ onLinkClick }: { onLinkClick: () => void }) {
   const cookies = useCookies();
   const pathname = usePathname();
   const router = useRouter();
@@ -62,10 +57,10 @@ export default function DashBoard({
             <Link
               key={i}
               href={tab.route}
+              onClick={onLinkClick}
               className={`${
                 activeTab === tab.tabIndex && "bg-secondary"
               } p-4 rounded-lg text-xl hover:bg-secondary w-full flex items-center gap-2`}
-              onClick={onLinkClick}
             >
               <FontAwesomeIcon icon={tab.icon} />
               <p>{tab.name}</p>
@@ -73,16 +68,14 @@ export default function DashBoard({
           ))}
         </div>
       </div>
-      <div className="flex flex-col gap-2 p-4 text-lg w-full mt-4">
-        <p className="text-lg">Pranshu Aggarwal</p>
-        <Link
-          href="/logout"
-          onClick={onLinkClick}
-          className="rounded-lg text-xl hover:bg-green-500 w-full flex items-center gap-2"
-        >
-          <FontAwesomeIcon icon={faSignOutAlt} />
-          <p>Sign Out</p>
-        </Link>
+      <div className="flex items-center gap-3 p-4 text-lg w-full mt-4">
+        <FontAwesomeIcon icon={faUser} />
+        <div>
+          <p className="text-lg">Pranshu Aggarwal</p>
+          <button onClick={() => { onLinkClick(); logout(); }} className="hover:cursor-pointer text-md">
+            Sign Out
+          </button>
+        </div>
       </div>
     </div>
   );
