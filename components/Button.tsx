5866fb7e48fc32a35826745efc689f5121fe4ad7
Pranshu1902 <pranshu1902@gmail.com>
2024-06-20 01:33:48 +0530

    update landing page

--- components/Button.tsx
+++ components/Button.tsx
@@ -16,12 +16,16 @@ export default function Button(props: ButtonProps) {
       className={`${
         props.type === "primary"
           ? "bg-primary text-white"
+          : props.type === "secondary"
+          ? "bg-white"
           : props.type === "delete"
           ? "bg-red-500 text-white"
           : props.type === "cancel"
           ? "bg-gray-200"
           : "border border-primary"
-      } font-bold p-2 rounded-lg px-12 py-2 flex justify-center items-center w-full gap-2 ${props?.className}`}
+      } font-bold p-2 rounded-lg px-12 py-2 flex justify-center items-center w-full gap-2 ${
+        props?.className
+      }`}
       onClick={props.onClick}
       type={props?.buttonType}
     >
--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,32 +1,44 @@
 import Image from "next/image";
-import logo from "@assets/plain_logo.png";
+import home from "@assets/home/home.png";
 import Link from "next/link";
+import Button from "./Button";
+import Logo from "./Logo";
 
 export default function LandingPage() {
   return (
-    <div className="p-4">
+    <div>
       {/* Header */}
-      <div className="flex justify-between">
+      <div className="flex justify-between bg-primary p-3">
         <div className="flex items-center gap-6">
-        <Image src={logo} alt="logo" width={50}></Image>
-        <p>JobPulse</p>
-        <a href="#about">About Us</a>
+          <Logo />
         </div>
         <div className="flex items-center gap-4">
-          <Link href={"/login"} className="p-2 rounded-lg">
-            Login
+          <Link href={"/login"}>
+            <Button type="primary" text="Login" />
           </Link>
-          <Link
-            href={"/signup"}
-            className="p-2 rounded-lg border border-purple-600"
-          >
-            Signup
+          <Link href={"/signup"}>
+            <Button type="secondary" text="Signup" />
           </Link>
         </div>
       </div>
-
-      <div className="flex flex-col mt-6">
-        <p className="text-6xl font-bold p-6">Stay Ahead of the Game with JobPulse</p>
+      <div className="p-4">
+        <div className="flex flex-col mt-6">
+          <p className="text-6xl font-bold">
+            Stay Ahead of the Game with JobPulse
+          </p>
+        </div>
+        <div className="mt-12">
+          <div className="flex flex-col md:flex-row items-center gap-4">
+            <Image
+              className="w-1/2 rounded-lg shadow"
+              src={home}
+              alt="home page"
+            />
+            <p className="text-2xl flex justify-center w-1/2">
+              Interactive Dashboards!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
