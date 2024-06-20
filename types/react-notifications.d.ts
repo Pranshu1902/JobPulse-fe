821519d41de6fd093ba235fa3de7714209cfe264
Pranshu1902 <pranshu1902@gmail.com>
2024-06-20 22:28:36 +0530

    resolve types error for notifications package

---
+++ types/react-notifications.d.ts
@@ -0,0 +1,7 @@
+// react-notifications.d.ts
+
+declare module "react-notifications" {
+  // Use 'any' type to bypass TypeScript checks
+  const NotificationManager: any;
+  export { NotificationManager };
+}
