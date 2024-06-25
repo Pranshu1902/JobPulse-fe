36192fcb48088bdb0c068bc950bb9de42e74fcd7
Pranshu1902 <pranshu1902@gmail.com>
2024-06-26 03:18:28 +0530

    integrate next-auth session with useAuth

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -4,10 +4,15 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { AuthUser } from "@models/models";
 import { useCookies } from "next-client-cookies";
 import { request } from "@api/fetch";
+import { signOut, useSession } from "next-auth/react";
 
 interface AuthContextType {
   user: AuthUser | null;
   getToken: () => string | undefined;
+  isAuthenticated: () => boolean;
+  logOut: () => void;
+  isLoading: boolean;
+  refetch: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -16,26 +21,70 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const cookies = useCookies();
+  const { data: session } = useSession();
   const [user, setUser] = useState<AuthUser | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUser = async () => {
+    setLoading(true);
+
+    // token stored in cookies
+    if (cookies.get("token")) {
+      const token = cookies.get("token");
+      const response = await request("GET", {}, "/current-user/", token);
+      setUser({ ...response, token });
+    }
+
+    // user logged in through social media
+    else if (session && session.user) {
+      setUser({
+        id: (session.user as any).id,
+        first_name: (session.user as any).first_name ?? "",
+        last_name: (session.user as any).last_name ?? "",
+        image: session.user.image ?? "",
+        username: (session.user as any).username,
+        email: session.user.email ?? "",
+        token: (session.user as any).authToken,
+      });
+    } else {
+      setUser(null);
+    }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const fetchUser = async () => {
-      const token = cookies.get("token");
-      if (token) {
-        const response = await request("GET", {}, "/current-user/", token);
-        setUser({ ...response, token });
-      }
-    };
-
-    if (!user && cookies.get("token")) {
+    if (!user && (cookies.get("token") || session?.user)) {
       fetchUser();
     }
-  }, [cookies, user]);
+  }, [cookies, user, session]);
 
-  const getToken = () => user?.token;
+  const getToken = () => user?.token ?? "";
+
+  const isAuthenticated = () => !!user;
+
+  const refetch = async () => {
+    const response = await request("GET", {}, "/current-user/", user?.token);
+    setUser({ ...response, token: user?.token });
+  };
+
+  const logOut = () => {
+    if (cookies.get("token")) {
+      cookies.remove("token");
+    }
+    signOut();
+  };
 
   return (
-    <AuthContext.Provider value={{ user, getToken }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        getToken,
+        isAuthenticated,
+        logOut,
+        isLoading: loading,
+        refetch,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
