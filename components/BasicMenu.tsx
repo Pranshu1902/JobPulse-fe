0597b93dd865c58f126b4a23eb41d7ea0462a8d7
Pranshu1902 <pranshu1902@gmail.com>
2024-06-28 22:56:47 +0530

    add menu component for comnents

---
+++ components/BasicMenu.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faEllipsis, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { JobComment } from "@models/models";
+
+interface BasicMenuProps {
+  comment: JobComment;
+  onDelete: (e: string) => void;
+  onEdit: (id: string, comment: string) => void;
+}
+
+export default function BasicMenu(props: BasicMenuProps) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        className="hover:text-primary hover:bg-tertiary rounded-full text-2xl px-2"
+      >
+        <FontAwesomeIcon icon={faEllipsis} />
+      </button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem
+          onClick={() => {
+            props.onEdit(props.comment.id, props.comment.comment);
+            handleClose();
+          }}
+          className="flex gap-2"
+        >
+          <FontAwesomeIcon icon={faEdit} /> Edit
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            props.onDelete(props.comment.id);
+            handleClose();
+          }}
+          className="flex gap-2"
+        >
+          <FontAwesomeIcon icon={faTrash} className="text-red-500" /> Delete
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
